b3396f1833031b19d5878616328803eb30921163
Garibaldi <chanokin@gmail.com>
2024-11-22 16:45:46 +0000

    step by step debug

--- debug_addition_nir/to_nir.py
+++ debug_addition_nir/to_nir.py
@@ -1,10 +1,18 @@
 import norse
 from modules.minimal_net import AdditionNetwork
+import nirtorch as nt
+import torch
 
 
 def convert_to_nir():
     net = AdditionNetwork()
     norse.to_nir(net, "addition_nir")
+
+
+def get_graph():
+    net = AdditionNetwork()
+    sample = torch.randn(1, 1)
+    nt.extract_torch_graph(net, sample, "addition_nir")
 
 
 if __name__ == "__main__":
