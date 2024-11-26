fc2c515910da37144632d0b735f2aefa020d891d
Garibaldi <chanokin@gmail.com>
2024-11-26 15:45:02 +0000

    testing norse modules with fx graph

--- debug_addition_nir/fx_graph.py
+++ debug_addition_nir/fx_graph.py
@@ -1,6 +1,49 @@
+from collections import OrderedDict
 import torch
-from torch.fx import symbolic_trace
+from torch.fx import Tracer, GraphModule
 import norse
+
+"""
+from: https://github.com/pytorch/pytorch/issues/51803
+"""
+
+
+class CustomisableTracer(Tracer):
+    """
+    ``Tracer`` is the class that implements the symbolic tracing functionality
+    of ``torch.fx.symbolic_trace``. A call to ``symbolic_trace(m)`` is equivalent
+    to ``Tracer().trace(m)``.
+    This Tracer override the ``is_leaf_module`` function to make symbolic trace
+    right in some cases.
+    """
+
+    def __init__(self, *args, customed_leaf_module=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.customed_leaf_module = customed_leaf_module
+
+    def is_leaf_module(self, m: torch.nn.Module, module_qualified_name: str) -> bool:
+        """
+        A method to specify whether a given ``nn.Module`` is a "leaf" module.
+        Leaf modules are the atomic units that appear in
+        the IR, referenced by ``call_module`` calls. By default,
+        Modules in the PyTorch standard library namespace (torch.nn)
+        are leaf modules. All other modules are traced through and
+        their constituent ops are recorded, unless specified otherwise
+        via this parameter.
+        Args:
+            m (Module): The module being queried about
+            module_qualified_name (str): The path to root of this module. For example,
+                if you have a module hierarchy where submodule ``foo`` contains
+                submodule ``bar``, which contains submodule ``baz``, that module will
+                appear with the qualified name ``foo.bar.baz`` here.
+        """
+        if self.customed_leaf_module and isinstance(m, self.customed_leaf_module):
+            return True
+
+        if hasattr(m, "_is_leaf_module") and m._is_leaf_module:
+            return True
+
+        return m.__module__.startswith("torch.nn") and not isinstance(m, torch.nn.Sequential)
 
 
 # Create a simple model
@@ -18,10 +61,20 @@ class Model(torch.nn.Module):
 
 def test_fx_trace():
     model = Model()
-    x = torch.rand(1, 1)
+    tracer = CustomisableTracer(customed_leaf_module=(norse.torch.LICell,))
+    tracer_graph = tracer.trace(model)
+    traced_model = GraphModule(tracer.root, tracer_graph, "test_fx_network")
 
-    traced = symbolic_trace(model)
-    print(traced.graph)
+    graph = traced_model.graph
+    print(graph)
+    named_modules = OrderedDict(traced_model.named_modules())
+
+    # two internal modules and the model itself
+    assert len(named_modules) == 3
+    assert "conv" in named_modules
+    assert "li" in named_modules
+
+    print(named_modules)
 
 
 if __name__ == "__main__":
